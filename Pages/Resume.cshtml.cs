348c691b346bc14365987b7e4bb916ed4372d62d
GabrielDR003 <gabrieldiazrivera003@gmail.com>
2025-08-08 10:52:34 -0700

    updated color scheme

--- Pages/Resume.cshtml.cs
+++ Pages/Resume.cshtml.cs
@@ -3,11 +3,11 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace MyPortfolioSite.Pages;
 
-public class PrivacyModel : PageModel
+public class ResumeModelModel : PageModel
 {
-    private readonly ILogger<PrivacyModel> _logger;
+    private readonly ILogger<ResumeModelModel> _logger;
 
-    public PrivacyModel(ILogger<PrivacyModel> logger)
+    public ResumeModelModel(ILogger<ResumeModelModel> logger)
     {
         _logger = logger;
     }
